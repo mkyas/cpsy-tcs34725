089311ed9d0dc2da6d94850bb700bb722fe9c505
Marcel Kyas <marcel@ru.is>
2024-09-20 09:49:05 +0000

    Test setting integration time.

--- testtcs.cpp
+++ testtcs.cpp
@@ -4,12 +4,12 @@
 
 int main()
 {
+	float r, g, b;
 	TCS34725 sensor;
-	std::array<std::uint16_t, 4> result{};
-	sensor.get_raw(result);
-	for (auto v : result) {
-		std::cout << v << " ";
-	}
-	std::cout << std::endl;
+
+	sensor.set_integration_time(2.4 * 8);
+	sensor.set_gain(TCS34725::GAIN_1X);
+	sensor.get_rgb(r, g, b);
+	std::cout << r << " " << g << " " << b << " " << std::endl;
 	return 0;
 }
